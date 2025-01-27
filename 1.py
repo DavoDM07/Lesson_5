89b9525969abcc1c7eb0462c40eb4131b48dd59a
DavoDM07 <davitmanukyan1236@gmail.com>
2025-01-27 04:27:21 +0400

    Work with for loop

---
+++ 1.py
@@ -0,0 +1,19 @@
+# Write a Python program to multiply all the items in a list.
+x = [1,2,3,4]
+a = 1
+for num in x:
+    a *= num
+print(a)
+
+x = [5,7,3,9,11]
+a = 1
+for num in x:
+    a *= num
+print(a)
+
+
+x = [25,1,1,13]
+a = 1
+for num in  x:
+    a *= num
+print(a)
---
+++ 3.py
@@ -0,0 +1,23 @@
+#Write a Python program to get the smallest number from a list.
+x = [1,2,3,4]
+a = x[0]
+for num in x:
+    if num < a:
+        a = num
+print(a)
+
+x = [5,7,3,9,11]
+a = x[0]
+for num in x:
+    if num < a:
+        a = num
+print(a)
+
+x = [25,1,1,3]
+a = x[0]
+for num in x:
+    if num < a:
+        a = num
+print(a)
+
+
